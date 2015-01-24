32d574d569ade1709219b09f12401f3a7b1f0a0a
Aniket Saoji <saoji.bc.edu>
2015-01-24 14:44:15 -0500

    Yo this is Boostrapped

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,17 @@ class TweetsController < ApplicationController
 		@tweet = Tweet.new
 	end
 
+	def create
+		@tweet = Tweet.create(tweet_params)
+		flash.now[:success]  = "Yo dawg, you created a Tweet"
+		render 'new'
+	end
+
+	private
+
+	def tweet_params
+		params.require(:tweet).permit(:content)
+	end
+
+
 end
