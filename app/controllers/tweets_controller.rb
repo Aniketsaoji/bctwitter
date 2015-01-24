9009a36e889b6b6a46996da1f890ecd6a22ef2e6
Aniket Saoji <saoji.bc.edu>
2015-01-24 18:03:45 -0500

    Yo

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,14 @@
 class TweetsController < ApplicationController
 
+	before_action :authenticate_user!
 	def new 
 		@tweet = Tweet.new
 	end
 
 	def create
+
 		@tweet = Tweet.create(tweet_params)
+		@tweet.user = current_user
 		flash.now[:success]  = "Yo dawg, you created a Tweet"
 		render 'new'
 	end
