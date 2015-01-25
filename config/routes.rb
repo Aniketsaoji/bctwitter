eb5c0a1e4b483a1f93a105e181fcf3dffba9a885
Aniket Saoji <saoji.bc.edu>
2015-01-25 14:04:35 -0500

    Did a lot of stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'static_pages#index'
   resources :tweets
+  devise_for :users
+
+  resources :profiles
   
 
   # Example of regular route:
