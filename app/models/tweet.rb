edb3ec84cee8fddc1086c107047cc516bbae2f02
Aniket Saoji <saoji.bc.edu>
2015-01-25 17:46:07 -0500

    Yo

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -10,24 +10,25 @@ class Tweet < ActiveRecord::Base
 
 	validate :hashtags_created
 	
-	def hashtags_created
-		begin
-		transaction do
-		@hashtags = self.extract_hash_tags
+		def hashtags_created
+			begin
+				transaction do
+					@hashtags = self.extract_hash_tags
 
-		@hashtags.each do |the_hashtag|
-			if Hashtag.where(tag: the_hashtag).any?
-			else 
-				if Hashtag.create(tag: the_hashtag)
-				else
-					self.errors.add(:tweet, "The hashtag is invalid")
+					@hashtags.each do |the_hashtag|
+					if Hashtag.where(tage: the_hashtag).any?
+					
+					else 
+						if Hashtag.create(tage: the_hashtag)
+						
+						else
+							self.errors.add(:tweet, "The hashtag is invalid")
+						end
+					end
 				end
-
-			end
 		end
-	end 
-	 rescue
-	 	self.errors.add(:tweet, "The hashtags are invalid.")
-	 end
+		rescue
+			self.errors.add(:tweet, "The hashtags are invalid.")
+		end
 	end 
 end
