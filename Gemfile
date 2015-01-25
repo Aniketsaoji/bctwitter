def988cde625965eba25fbcbf39abe618839a617
Aniket Saoji <saoji.bc.edu>
2015-01-25 17:16:02 -0500

    Sup

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'devise'
 
 gem 'carrierwave'
 
+gem 'twitter-text', '~> 1.10.0'
+
 
 group :development, :test do
 	# Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125211930) do
+ActiveRecord::Schema.define(version: 20150125213809) do
+
+  create_table "hashtags", force: :cascade do |t|
+    t.string   "tage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "picture"
