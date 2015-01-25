913bec6c78690acb3e18109473537d3f6d5b48a5
Aniket Saoji <saoji.bc.edu>
2015-01-25 12:59:28 -0500

    Made some cool stuff

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,14 +3,23 @@ class TweetsController < ApplicationController
 	before_action :authenticate_user!
 	def new 
 		@tweet = Tweet.new
+		@tweets = current_user.tweets
 	end
 
 	def create
 
 		@tweet = Tweet.create(tweet_params)
 		@tweet.user = current_user
+		@tweet.save
+
+		@tweets = current_user.tweets
+
 		flash.now[:success]  = "Yo dawg, you created a Tweet"
 		render 'new'
+	end
+
+	def index
+		@tweets = Tweet.all.reject{ |tweet| tweet.user == current_user}
 	end
 
 	private
